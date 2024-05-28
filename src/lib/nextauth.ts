5a06104efe79208df957abf6605030c51182837c
Khanhnv2403 <vietkhanh.fe.work@gmail.com>
2024-05-28 20:45:23 +0700

    check err #4

--- src/lib/nextauth.ts
+++ src/lib/nextauth.ts
@@ -25,7 +25,7 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
-  secret: "fghsfghgfhgsfh",
+  // secret: "fghsfghgfhgsfh",
   // callbacks: {
   //   jwt: async ({ token }) => {
   //     const db_user = await prisma.user.findFirst({
